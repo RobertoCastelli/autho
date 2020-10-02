b7328036d84659545104b403db09db8b4c74fa6e
RobertoCastelli <roberto.castelli.teal@gmail.com>
2020-10-02 21:57:17 +0200

    first commit

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBND5g7vAy3CuZm2qmw5cQYCusRDZjJt6k",
+  authDomain: "autho-ff98f.firebaseapp.com",
+  databaseURL: "https://autho-ff98f.firebaseio.com",
+  projectId: "autho-ff98f",
+  storageBucket: "autho-ff98f.appspot.com",
+  messagingSenderId: "30674595755",
+  appId: "1:30674595755:web:08f494b51873f3e297ecee",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.auth();
+
+export default firebaseConfig;
