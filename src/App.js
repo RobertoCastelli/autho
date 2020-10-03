045b11b86cbdfeb6c2bac2a14c0b917ae0d50828
RobertoCastelli <roberto.castelli.teal@gmail.com>
2020-10-03 11:02:01 +0200

    sign in out up all working

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import SignIn from "./components/SignIn";
 import SignOut from "./components/SignOut";
 import SignUp from "./components/SignUp";
 import Header from "./components/Header";
-import Footer from './components/Footer'
+import Footer from "./components/Footer";
 import ContextProvider from "./context/context";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <ContextProvider>
-        <Header />
-        <SignUp />
-        <SignIn />
-        <SignOut />
-        <Footer /> 
-      </ContextProvider>
-    </Router>
+    <ContextProvider>
+      <Header />
+      <SignUp />
+      <SignIn />
+      <SignOut />
+      <Footer />
+    </ContextProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { FaReact } from "react-icons/fa";
+import { SiFirebase } from "react-icons/si";
 
 const Header = () => {
   return (
     <header>
-      <h1>AUTH</h1>
-      <h3>React + Firebase authentication test</h3>
+      <h1>
+        <FaReact /> AUTH <SiFirebase />
+      </h1>
+      <h3>React & Firebase authentication</h3>
       <small>back to the guide</small>
     </header>
   );
